8271489a0629b0fe37d41bbc7317d697b04d704f
karthik svs <svskarthik98@gmail.com>
2019-11-06 15:16:14 +0530

    Merge and quick sort

---
+++ MergeANDQuick Sort/src/Ass2/Solution.java
@@ -0,0 +1,37 @@
+package Ass2;
+class Solution{
+	public static String[] mergeSort(String[] arr){
+		// fill you code Here
+		if (arr.length >= 2) {
+            String[] left = new String[arr.length / 2];
+            String[] right = new String[arr.length - arr.length / 2];
+
+            for (int i = 0; i < left.length; i++) {
+                left[i] = arr[i];
+            }
+
+            for (int i = 0; i < right.length; i++) {
+                right[i] = arr[i + arr.length / 2];
+            }
+
+            mergeSort(left);
+            mergeSort(right);
+            merge(arr, left, right);
+        }
+		return arr;
+    }
+
+    public static void merge(String[] arr, String[] left, String[] right) {
+        int a = 0;
+        int b = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (b >= right.length || (a < left.length && left[a].compareToIgnoreCase(right[b]) < 0)) {
+                arr[i] = left[a];
+                a++;
+            } else {
+                arr[i] = right[b];
+                b++;
+            }
+        }
+    }
+}
